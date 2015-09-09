62e4d9cd2d7bf0ff8eecbd31a43dfbf1025f79a0
Niklas Bieck <niklas.bieck@digipen.edu>
2015-09-09 16:47:35 -0700

    Finish Texture interface

--- src/CS562/GLWrapper/Texture.h
+++ src/CS562/GLWrapper/Texture.h
@@ -11,9 +11,24 @@
 
 namespace CS562
 {
-	enum class ImageFormat
+	enum class TextureFormatInternal
 	{
 		//add formats relevant to us here
+	};
+
+	enum class TextureFormat
+	{
+
+	};
+
+	enum class TextureDataType
+	{
+
+	};
+
+	enum class TextureParameter
+	{
+
 	};
 
 	// This manages the GPU-side texture resource.
@@ -24,15 +39,21 @@ namespace CS562
 		Texture();
 		~Texture();
 
-		void AllocateSpace(unsigned width, unsigned height, ImageFormat format, unsigned mip_levels);
+		void AllocateSpace(unsigned width, unsigned height, TextureFormatInternal format, unsigned mip_levels);
 
 		//parameters
+		void SetParameter(TextureParameter param, float value);
+		void SetParameter(TextureParameter param, int value);
 
 		//Binding - Unbinding
 		Unbinder<Texture> Bind(unsigned bind_location);
 		void Unbind();
 
 		//Data transfer
+		void TransferData(int x_offset, int y_offset, unsigned width, unsigned height, 
+			TextureFormat format, TextureDataType type, int mip_level = 0);
+
+		void GenerateMipMaps();
 
 	private:
 
