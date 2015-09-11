6deeb3783d35d8f2a24c0e6fa114b798249d01b0
Niklas Bieck <niklas.bieck@digipen.edu>
2015-09-10 19:58:20 -0700

    Remove old mesh loading code

--- src/CS562/ResourceLoader/ResourceLoader.h
+++ src/CS562/ResourceLoader/ResourceLoader.h
@@ -25,41 +25,7 @@ namespace CS562
 
 		static std::shared_ptr<ShaderProgram> LoadShaderProgramFromFile(const char* filename);
 
-		static void LoadGeometryFromFile(const char* filename, 
-										 std::vector<Vertex>& vertex_buffer, 
-										 std::vector<unsigned>& index_buffer);
-
-		static std::shared_ptr<Geometry> LoadGeometryFromFile(const char* filename);
-
 	private:
-		
-		//this class is used to help in loading .obj files
-		class ObjLoaderHelper
-		{
-		public:
-
-			ObjLoaderHelper();
-
-			unsigned GetIndex(const Vertex& vtx);
-			std::vector<Vertex> GetVertexBuffer();
-
-		private:
-
-			std::map<Vertex, unsigned> vertex_to_index_;
-			std::vector<Vertex> vertex_buffer_;
-			unsigned next_index_;
-		};
-
-		//these functions load individual parts of an .obj file
-		static void LoadPosition(std::istringstream& line, 
-								 std::vector<Vertex::Position>& positions);
-		static void LoadNormal(std::istringstream& line, 
-							   std::vector<Vertex::Normal>& normals);
-		static void LoadFace(std::istringstream& line, 
-							 ObjLoaderHelper& helper, 
-							 const std::vector<Vertex::Position>& positions, 
-							 const std::vector<Vertex::Normal>& normals, 
-							 std::vector<unsigned>& index_buffer);
 
 	};
 }
